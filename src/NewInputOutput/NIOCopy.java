5a2ae25653f3b4a5afc96c116fa788d7d6cd4c26
Taufik <MTaufik@Outlook.Com>
2022-06-17 03:50:50 +0700

    New I/O

---
+++ src/NewInputOutput/NIOCopy.java
@@ -0,0 +1,30 @@
+package NewInputOutput;
+
+import java.io.*;
+import java.nio.*;
+import java.nio.channels.*;
+import java.nio.file.*;
+
+//Copy a file using NIO.
+public class NIOCopy {
+	public static void main(String[] args) {
+
+		if (args.length != 2) {
+			System.out.println("Usage: Copy from to");
+			return;
+		}
+
+		try {
+			Path source = Path.of(args[0]);
+			Path target = Path.of(args[1]);
+
+			// Copy the file.
+			Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
+
+		} catch (InvalidPathException e) {
+			System.out.println("Path Error " + e);
+		} catch (IOException e) {
+			System.out.println("I/O Error " + e);
+		}
+	}
+}
